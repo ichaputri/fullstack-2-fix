2708ed7834c4dedf1e55785960e71a824fc7ab1a
ichaputri <ichaputriana39@gmail.com>
2021-09-14 15:30:35 +0700

    first commit

---
+++ backend/data/Products.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+      name: "Kaos Luffy",
+      imageUrl:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lazada.co.id%2Fproducts%2Fav-deptstore-kaos-anime-lengan-panjang-baju-anime-lengan-panjang-kaos-anime-baju-anime-kaos-one-piece-kaos-luffy-kaos-pria-distro-baju-distro-kaos-dewasa-i4844984435.html&psig=AOvVaw0SvfDFJS7qtiABNAnUyhtZ&ust=1631692823934000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCNi83__fICFQAAAAAdAAAAABAJ",
+      description:
+        "Kaos Santai Gambar Luffy, Bahan dingin",
+      price: 34000,
+      countInStock: 15,
+    },
+    {
+      name: "Kaos Inumaki Toge",
+      imageUrl:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tokopedia.com%2Fkakkoiicollection%2Fkaos-jujutsu-inumaki-toge-baju-anime-jujutsu-kaisen-putih-s&psig=AOvVaw0SvfDFJS7qtiABNAnUyhtZ&ust=1631692823934000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCNi83__fICFQAAAAAdAAAAABAm",
+      description:
+        "Kaos Santai Anime Gambar Inumaki Toge, Bahan Dingin, Kualitas Terjamin",
+      price: 100000,
+      countInStock: 10,
+    },
+    {
+      name: "Jersey MSBY, Haikyu",
+      imageUrl:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tokopedia.com%2Faquaasia%2Fjersey-stelan-msby-haikyu-baju-jersey-celana-bisa-custome-nama&psig=AOvVaw34FASRTASssk1AHRY7T6RZ&ust=1631693113652000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiE4-aA_vICFQAAAAAdAAAAABAD",
+      description:
+        "Jersey Tim MSBY, Kualitas Terjamin.",
+      price: 185000,
+      countInStock: 5,
+    },
+    {
+      name: "Jersey Voli Tim Itachiyama",
+      imageUrl:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fshopee.co.id%2FJersey-Voli-Setelan-Baju-Kaos-Costume-Anime-Haikyuu-Itachiyama-Team-Vollyball-i.313058927.3570410632&psig=AOvVaw3tfnSYeJG0VsD3fq4mxAyx&ust=1631693264902000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjurJ-B_vICFQAAAAAdAAAAABAD",
+      description:
+        "Jersey Tim Itachiyama, Haikyu.",
+      price: 185000,
+      countInStock: 25,
+    },
+    {
+      name: "Kamomedai Jersey",
+      imageUrl:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fshopee.co.id%2FKaos-Cosplay-Anime-Haikyuu-Jersey-Volleyball-Kamomedai-i.7819310.6558172590&psig=AOvVaw2dvgj3tuI7GR-6eCMr_Ym7&ust=1631693402216000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPi03OCB_vICFQAAAAAdAAAAABAD",
+      description:
+        "Kualitas Terjamin, Bahan Dingin.",
+      price: 180000,
+      countInStock: 4,
+    },
+    {
+      name: "Schweiden Jersey",
+      imageUrl:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tokopedia.com%2Fprodotashop%2Fkustom-anime-cosplay-haikyuu-kaos-baju-jersey-schweiden-adler-volley&psig=AOvVaw2rBrhlp6EbGoE6HDhb1nU8&ust=1631693486027000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOjxhomC_vICFQAAAAAdAAAAABAJ",
+      description:
+        "Setelan Jersey Tim Schweiden, Kualitas terjamin.",
+      price: 180000,
+      countInStock: 10,
+    },
+  ];
+  
+  module.exports = products;    
