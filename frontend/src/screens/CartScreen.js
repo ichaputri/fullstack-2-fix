52458ac8ac0d3695cefb83ec18a0d207dcc940e3
ichaputri <ichaputriana39@gmail.com>
2021-09-22 11:16:07 +0700

    part 3 22-09-2021

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,11 @@
+import './CartScreen.css';
+
+const CartScreen = () => {
+    return(
+        <div className = "cartcreen">
+            Cart Screen
+        </div>
+    )
+}
+
+export default CartScreen;
